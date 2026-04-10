6d2d408eded5df5026cc0ec1104927f14b4ff904
Sherly Phangestu <phangestusherly@gmail.com>
2026-04-11 06:16:15 +0700

    refactor: Move dashboard view with dashboard route

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        dashboard: resolve(__dirname, 'dashboard/index.html'),
       },
     },
   },
